310df966c4d9cc89ebd3952d501e2ad0d9d7a0a3
Marco N <sainoba@gmail.com>
2016-04-15 19:20:27 -0500

    Se modificó la configuración del archivo wsgi_webfaction.py para que pudiera ejecutarse el servidor Webfaction

---
+++ Mabruka/wsgi_webfaction.py
@@ -0,0 +1,36 @@
+"""
+WSGI config for Mabruka project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+
+import os
+import sys
+import site
+
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+
+# Add the site-packages of the chosen virtualenv to work with
+site.addsitedir(
+    '/home/sainoba/.virtualenvs/Mabruka/lib/python3.5/site-packages')
+
+# Add the app's directory to the PYTHONPATH
+sys.path.append('/home/sainoba/webapps/mabruka_wsgi/Mabruka')
+sys.path.append('/home/sainoba/webapps/mabruka_wsgi/Mabruka/Mabruka')
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Mabruka.settings.staging")
+
+activate_env = os.path.expanduser(
+    "/home/sainoba/.virtualenvs/Mabruka/bin/activate_this.py")
+# execfile(activate_env, dict(__file__=activate_env))
+
+with open(activate_env) as f:
+    code = compile(f.read(), activate_env, 'exec')
+    exec(code, dict(__file__=activate_env))
+
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
